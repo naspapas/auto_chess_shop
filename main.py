39dc0754b156354905e9202e63ffd9d49f8c973a
naspapas <nasiopapas@gmail.com>
2022-05-12 02:17:06 +1000

    fixed level odds, object logic and added ANSI colour

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ unit_e_two = Unit('E2', 1, 1)
 
 available_one_cost_units = [unit_a_one.name, unit_a_two.name]
 available_two_cost_units = [unit_b_one.name, unit_b_two.name]
-available_three_cost_units = [unit_a_one.name, unit_a_two.name]
-available_four_cost_units = [unit_a_one.name, unit_a_two.name]
-available_five_cost_units = [unit_a_one.name, unit_a_two.name]
+available_three_cost_units = [unit_c_one.name, unit_c_two.name]
+available_four_cost_units = [unit_d_one.name, unit_d_two.name]
+available_five_cost_units = [unit_e_one.name, unit_e_two.name]
 
 
 class Shop:
@@ -40,38 +40,54 @@ class Shop:
 
     # Method to set percentages for each level, interacted with by method: adjust_shop_chance
     def percentage_adjust(self, one_rank, two_rank, three_rank, four_rank, five_rank):
-        Shop.one_cost_chance = one_rank
-        Shop.two_cost_chance = two_rank
-        Shop.three_cost_chance = three_rank
-        Shop.four_cost_chance = four_rank
-        Shop.five_cost_chance = five_rank
+        self.one_cost_chance = one_rank
+        self.two_cost_chance = two_rank
+        self.three_cost_chance = three_rank
+        self.four_cost_chance = four_rank
+        self.five_cost_chance = five_rank
 
     def shop_percent(self, chosen_array):
-            Shop.shop_units.clear()
-            if random() < Shop.five_cost_chance:
-                chosen_array.append(available_five_cost_units[randint(0, len(available_five_cost_units))])
-            if random() < Shop.four_cost_chance:
-                chosen_array.append(available_four_cost_units[randint(0, len(available_four_cost_units))])
-            if random() < Shop.three_cost_chance:
-                chosen_array.append(available_three_cost_units[randint(0, len(available_three_cost_units))])
-            if random() < Shop.two_cost_chance:
-                chosen_array.append(available_two_cost_units[randint(0, len(available_two_cost_units))])
+
+            if random() < self.five_cost_chance:
+                chosen_array.append(available_five_cost_units[randint(0, 1)])
+            elif random() < self.four_cost_chance:
+                chosen_array.append(available_four_cost_units[randint(0, 1)])
+            elif random() < self.three_cost_chance:
+                chosen_array.append(available_three_cost_units[randint(0, 1)])
+            elif random() < self.two_cost_chance:
+                chosen_array.append(available_two_cost_units[randint(0, 1)])
             else:
                 chosen_array.append(available_one_cost_units[randint(0, 1)])
 
 
     def turn_start_shop(self):
+        i = 0
         while Shop.counter <= 4:
             Shop.shop_percent(self, Shop.shop_units)
             Shop.counter += 1
-        print(Shop.shop_units)
+        while i <= 4:
+            if Shop.shop_units[i].startswith('A'):
+                print(f"{Shop.shop_units[i]}", end=" ")
+            if Shop.shop_units[i].startswith('B'):
+                print(f"\033[32m{Shop.shop_units[i]}\033[0m", end=" ")
+            if Shop.shop_units[i].startswith('C'):
+                print(f"\033[34m{Shop.shop_units[i]}\033[0m", end=" ")
+            if Shop.shop_units[i].startswith('D'):
+                print(f"\033[35m{Shop.shop_units[i]}\033[0m", end=" ")
+            if Shop.shop_units[i].startswith('E'):
+                print(f"\033[33m{Shop.shop_units[i]}\033[0m", end=" ")
+
+            i += 1
 
     def randomize_shop(self):
-
+        if Shop.counter == 5:
+            Shop.shop_units.clear()
+            Shop.counter = 0
         while Shop.counter <= 4:
             Shop.shop_percent(self, Shop.shop_units)
             Shop.counter += 1
-        print(Shop.shop_units)
+        p1.money -= 2
+
 
     def buy_exp(self):
         if p1.money >= 4:
@@ -81,21 +97,21 @@ class Shop:
             print("You don't have enough money to level up!")
 
     def adjust_shop_chance(self):
-        match p1.level:
+        match self.player.level:
             case 3:
-                Shop.percentage_adjust(Shop, .75, .25, 0, 0, 0)
+                self.percentage_adjust(.75, .25, 0, 0, 0)
             case 4:
-                Shop.percentage_adjust(Shop, .55, .30, .15, 0, 0)
+                self.percentage_adjust(.55, .30, .15, 0, 0)
             case 5:
-                Shop.percentage_adjust(Shop, .45, .33, .20, .02, 0)
+                self.percentage_adjust(.45, .33, .20, .02, 0)
             case 6:
-                Shop.percentage_adjust(Shop, .25, .40, .30, .05, 0)
+                self.percentage_adjust(.25, .40, .30, .05, 0)
             case 7:
-                Shop.percentage_adjust(Shop, .19, .30, .35, .15, .01)
+                self.percentage_adjust(.19, .30, .35, .15, .01)
             case 8:
-                Shop.percentage_adjust(Shop, .16, .20, .35, .25, .04)
+                self.percentage_adjust(.16, .20, .35, .25, .04)
             case 9:
-                Shop.percentage_adjust(Shop, .09, .15, .30, .30, 16)
+                self.percentage_adjust(.09, .15, .30, .30, 16)
 
 
 def adjust_required_exp():
@@ -124,30 +140,33 @@ class Player:
         self.lvl_up_xp_required = 2
 
     def level_up(self):
-        if p1.exp >= p1.lvl_up_xp_required:
-            p1.level += 1
-            p1.exp = 0
+        if self.exp >= self.lvl_up_xp_required:
+            self.level += 1
+            self.exp = 0
 
 
 p1 = Player()
 
 
-def gameplay_loop(self):
+def gameplay_loop():
+    p1_shop = Shop(p1)
     while True:
-        Shop.turn_start_shop(Shop)
-        print(f"You are level {p1.level}, {p1.exp}/{p1.lvl_up_xp_required}")
-        print(f'You have ${p1.money}')
-        Player.level_up(p1)
+        p1_shop.turn_start_shop()
+        print(f"\nYou are level {p1.level}, {p1.exp}/{p1.lvl_up_xp_required}")
+        print(f'You have \033[32;1;4m${p1.money}\033[0m')
+        p1.level_up()
         adjust_required_exp()
+        p1_shop.adjust_shop_chance()
         user_input = input('> ')
-        if user_input == 'level':
-            Shop.buy_exp(p1)
 
-        if user_input == 'reroll':
-            Shop.randomize_shop(Shop)
-
-    Shop.counter = 0
+        if user_input.lower() == 'level':
+            p1_shop.buy_exp()
+        elif user_input == 'reroll':
+            p1_shop.randomize_shop()
+        else:
+            print("\033[31;1;4mInvalid Command!\033[0m")
+    p1_shop.counter = 0
 
 
 if __name__ == '__main__':
-    gameplay_loop(p1)
+    gameplay_loop()
